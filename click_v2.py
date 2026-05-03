3df0effdc77df84284e018107a36c784bacb2dce
jesmp <jmp.0711@yahoo.com>
2026-05-03 01:39:55 -0500

    updated notations in this program and also the variable names.

--- click_v2.py
+++ click_v2.py
@@ -2,16 +2,20 @@ import pyautogui
 import time
 import random
 
+print("Starting in 5 seconds...")
 time.sleep(5)
 
 while True:
-    x=random.randint(3, 300)
+    # Variables
+    x=random.randint(120,140)
     y=random.randint(500, 900)
     z=random.randint(0, 3)
-    i=random.randint(-10, 10)
-    a=random.randint(120,140)
-    pyautogui.moveTo(a, y, z)
-    print("next click will be in",x,"seconds")
+    random_time=random.randint(3, 300)
+    random_scroll=random.randint(-10, 10)
+
+    # Logic
+    pyautogui.moveTo(x, y, z)
+    print("next click will be in",random_time,"seconds")
     pyautogui.click()
-    pyautogui.scroll(i)
-    time.sleep(x)
+    pyautogui.scroll(random_scroll)
+    time.sleep(random_time)
