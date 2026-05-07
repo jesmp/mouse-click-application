8dbabcc5840667b5477e979be0c67c215a956fd8
jesmp <jmp.0711@yahoo.com>
2026-05-06 21:36:14 -0500

    made updates to give more control to the user.

--- click_v2.py
+++ click_v2.py
@@ -1,21 +1,38 @@
 import pyautogui
+from datetime import datetime, timedelta
 import time
 import random
 
-print("Starting in 5 seconds...")
-time.sleep(5)
+def get_amount_of_time():
+    """"This will get the amount of time the user wants the application to run for in minutes."""
+    input_time = input("Enter the amount of time you want the application to run for (in minutes): ")
+    end_time = datetime.now() + timedelta(minutes=int(input_time))
+    print(f"The application will run until {end_time.strftime('%Y-%m-%d %H:%M:%S')}")
+    return end_time
+    
+def start_clicking(end_time):
+    """This will start the clicking process."""
+    print("Starting the clicking process in 5 seconds...")
+    time.sleep(5)
 
-while True:
-    # Variables
-    x=random.randint(120,140)
-    y=random.randint(500, 900)
-    z=random.randint(0, 3)
-    random_time=random.randint(3, 300)
-    random_scroll=random.randint(-10, 10)
+    while datetime.now() < end_time:
+        # Variables
+        x=random.randint(120,140)
+        y=random.randint(500, 900)
+        z=random.randint(0, 3)
+        random_time=random.randint(3, 120)
+        random_scroll=random.randint(-10, 10)
 
-    # Logic
-    pyautogui.moveTo(x, y, z)
-    print("next click will be in",random_time,"seconds")
-    pyautogui.click()
-    pyautogui.scroll(random_scroll)
-    time.sleep(random_time)
+        # Logic
+        pyautogui.moveTo(x, y, z)
+        print("next click will be in",random_time,"seconds")
+        pyautogui.click()
+        pyautogui.scroll(random_scroll)
+        time.sleep(random_time)
+    
+    print("Time's up! Stopping the application.")
+
+
+if __name__ == "__main__":
+    end_time = get_amount_of_time()
+    start_clicking(end_time)
